1b2f8a4fce03558c42ea247a7fdd73767c297d1e
danielethi <danielethi@outlook.com>
2023-01-21 21:52:50 -0500

    object-commits

--- index.js
+++ index.js
@@ -1,48 +1,64 @@
 // //speical array method arguments
-// function fun(){
-//   console.log(arguments)
-// }
-// fun([1,2,3,4])//array
+function fun(){
+  console.log(arguments)
+}
+fun([1,2,3,4])//array
 
-// function fun(...args){
-//   console.log(args)
-// }
-// fun([1,2,3,4])//array
+function fun(...args){
+  console.log(args)
+}
+fun([1,2,3,4])//array
 
-// function fun(a,b,c,...args){
-//   console.log(args)
-// }
-// fun([1,2,3,4])//array
+function fun(a,b,c,...args){
+  console.log(args)
+}
+fun([1,2,3,4])//array
 
-// function fun(){
-//   for(let i=0;i<arguments.length;i++)
-//   console.log(arguments)
-// }
-// fun([1,2,3,4])//array
+function fun(){
+  for(let i=0;i<arguments.length;i++)
+  console.log(arguments)
+}
+fun([1,2,3,4])//array
 
-// function fun(arguments){//obj
-//   console.log(arguments)
-// }
-// fun([1,2,3,4])
+function fun(arguments){//obj
+  console.log(arguments)
+}
+fun([1,2,3,4])
 
 // //rest parameter
-// const arr = [1,2,3]
-// arr[1]=56
-// let arr2=[...arr]
-// console.log(arr)
-// console.log(arr2)
+const arr = [1,2,3]
+arr[1]=56
+let arr2=[...arr]
+console.log(arr)
+console.log(arr2)
 
 //looping thourhg objects
 const obj={
   name:"daniel",
   age:26
 }
-for(let elem of obj){
-  console.log(elem)
+console.log(Object.keys(obj))
+console.log(Object.values(obj))
+console.log(Object.entries(obj))
+
+const obj4=[{
+  name:"daniel",
+  age:26
+},
+{
+  name:"Doe",
+  age:29
+},
+{
+  name:"Eldana",
+  age:35
+},
+]
+
+for(let w of obj4){
+  console.log(w)
 }
-for(let elem in obj){
-  console.log(elem)
-}
+
 
 
 
